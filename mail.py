54b71eed082a080e9a041cc8041714c7ef360953
Mscheer75 <mscheer1@mail.csuchico.edu>
2020-12-10 22:25:16 +0000

    fixed some bugs

--- mail.py
+++ mail.py
@@ -4,11 +4,11 @@ import requests
 
 def send_message():
     return requests.post(
-        "https://api.mailgun.net/v3//messages",
-        auth=("api", "4879ff27-b1e850a6"),
-        data={"from": 'hello@example.com',
+        "https://api.mailgun.net/v3/sandbox286ad405f7fa4339a9aac3f8399cfaf5.mailgun.org/messages",
+        auth=("api", "42ab66f261dc8b52dff6f3bc3e4ddf39-4879ff27-b1e850a6"),
+        data={"from": "the turret <mailgun@sandbox286ad405f7fa4339a9aac3f8399cfaf5.mailgun.org>",
             "to": ["crazyhalolad@gmail.com"],
-            "subject": "You have a visitor",
-            "html": "<html> intruder is at your door being shot  </html>"})
+            "subject": "Target found",
+            "text": "intruder is at your door being shot"})
 
 
--- server.py
+++ server.py
@@ -6,14 +6,14 @@ kit = ServoKit(channels=16)
 
 def turnRight():
     kit.servo[1].angle = 45
-    time.sleep(4)
+    time.sleep(.5)
     kit.servo[1].angle = 0
-    time.sleep(4)
+    time.sleep(.5)
 def turnLeft(x):
     kit.servo[1].angle = 0
-    time.sleep(4)
+    time.sleep(.5)
     kit.servo[1].angle = x
-    time.sleep(4)
+    time.sleep(.5)
 
 
 def shoot():
